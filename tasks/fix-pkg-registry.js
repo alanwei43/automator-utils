65baadd6319acf0e46f98d46c7a38e8b9073e6aa
Alan <alan.wei@live.com>
2021-05-30 13:45:46 +0800

    build: 更新发布脚本支持gpr

---
+++ tasks/fix-pkg-registry.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+if (fs.existsSync(".npmrc")) {
+    fs.unlink(".npmrc");
+}
+const pkg = JSON.parse(fs.readFileSync("package.json", { encoding: "utf-8" }));
+pkg.publishConfig = {
+    "registry": "https://npm.pkg.github.com/@alanwei43"
+};
+fs.writeFileSync("package.json", JSON.stringify(pkg), {
+    encoding: "utf-8"
+});
