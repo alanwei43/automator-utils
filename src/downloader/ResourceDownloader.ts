cfdd440803a1522e011c406ae6e134d0f68fd16f
Alan <alan.wei@live.com>
2021-06-05 18:52:56 +0800

    feat: 重构资源下载类库

--- src/downloader/ResourceDownloader.ts
+++ src/downloader/ResourceDownloader.ts
@@ -15,48 +15,87 @@
  * manager.update();
  */
 
-import { parseYamlConfig } from "../utils";
+import { hash, parseYamlConfig } from "../utils";
 import { IHttpClient } from "../network";
 import path from "path";
 
-type ResourceManifest = {
+export type ResourceDownloaderManifest = {
+    /**
+     * 文件保存的根目录
+     */
     rootDir: string
+    /**
+     * URL前缀
+     */
     baseUrl?: string
+    /**
+     * 文件列表
+     */
     files: Array<{
+        /**
+         * 文件对应的URL
+         */
         url: string
-        name: string
-        dir: string
+        /**
+         * 文件名, 默认URL的文件名
+         */
+        name?: string
+        /**
+         * 文件保存路径, 结合 rootDir 
+         */
+        dir?: string
     }>
+}
+type ResourceDownloadResult = {
+    url: string
+    file: string
+    status: string
 }
 
 export class ResourceDownloader {
     constructor(
-        private http: IHttpClient,
-        private manifestUrl: string,
-        private manifestFormat: "yml" | "json"
+        private http: IHttpClient
     ) { }
     /**
-     * 更新资源
+     * 从远程更新资源
      */
-    async update(): Promise<boolean> {
-        const manifestContent = await this.http.getText(this.manifestUrl);
-        let manifestList: Array<ResourceManifest>;
-        if (this.manifestFormat === "yml") {
-            manifestList = parseYamlConfig<Array<ResourceManifest>>(manifestContent);
+    async downloadViaRemote(manifestUrl: string, manifestFormat: "yml" | "json"): Promise<Array<ResourceDownloadResult>> {
+        const manifestContent = await this.http.getText(manifestUrl);
+        let manifestList: Array<ResourceDownloaderManifest>;
+        if (manifestFormat === "yml") {
+            manifestList = parseYamlConfig<Array<ResourceDownloaderManifest>>(manifestContent);
         }
-        if (this.manifestFormat === "json") {
+        if (manifestFormat === "json") {
             manifestList = JSON.parse(manifestContent);
         }
+        return this.download(manifestList);
+    }
+
+    async download(manifestList: Array<ResourceDownloaderManifest>): Promise<Array<ResourceDownloadResult>> {
+        const list: Array<ResourceDownloadResult> = [];
         for (let manifest of manifestList) {
             if (!manifest || !manifest.rootDir) {
                 continue;
             }
             const dest = path.join(process.cwd(), manifest.rootDir);
             for (let file of manifest.files) {
-                const fileFullPath = path.join(dest, file.dir, file.name);
-                await this.http.saveAs(`${manifest.baseUrl || ""}${file.url}`, fileFullPath);
+                const fullUrl = `${manifest.baseUrl || ""}${file.url}`;
+                const fileName = file.name || path.basename(file.url) || hash(fullUrl);
+                const fileFullPath = path.join(dest, file.dir || "", fileName);
+                const result: ResourceDownloadResult = {
+                    url: fullUrl,
+                    file: fileFullPath,
+                    status: "ready"
+                };
+                list.push(result);
+                try {
+                    await this.http.saveAs(fullUrl, fileFullPath);
+                    result.status = "success";
+                } catch (err) {
+                    result.status = `error:${err.message}`;
+                }
             }
         }
-        return true;
+        return list;
     }
 }
---
+++ tests/downloader/ResourceDownloader.test.ts
@@ -0,0 +1,26 @@
+import { HttpClient, ResourceDownloader } from "../../src";
+import path from "path";
+import fs from "fs";
+
+test("资源下载 测试下载", async () => {
+    const downloader = new ResourceDownloader(HttpClient.InitClientBySimple());
+    const results = await downloader.download([{
+        "rootDir": "logs",
+        "baseUrl": "http://www.alanwei.com/",
+        "files": [{
+            "url": "README.md"
+        }, {
+            "url": "README.md",
+            "dir": "test",
+            "name": "new-name.md"
+        }]
+    }]);
+    expect(results.length).toBe(2);
+    expect(results[0].status).toBe("success");
+    expect(results[0].file).toBe(path.join(process.cwd(), "logs", "README.md"));
+    expect(results[1].status).toBe("success");
+    expect(results[1].file).toBe(path.join(process.cwd(), "logs", "test", "new-name.md"));
+
+    fs.unlinkSync(results[0].file);
+    fs.unlinkSync(results[1].file);
+});
