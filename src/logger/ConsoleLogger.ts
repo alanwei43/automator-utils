0a37ac649c9be82813f77ce56f95e3dd3aa8d1f8
Alan <alan.wei@live.com>
2021-08-15 10:54:05 +0800

    feat: 控制台日志输出时间

--- src/logger/ConsoleLogger.ts
+++ src/logger/ConsoleLogger.ts
@@ -2,12 +2,12 @@ import { ILogger } from "./ILogger";
 
 export class ConsoleLogger implements ILogger {
     debug(...args: any[]): void {
-        console.debug.apply(console, args);
+        console.debug.apply(console, [`[${new Date().toLocaleString()}]`, ...args]);
     }
     warn(...args: any[]): void {
-        console.warn.apply(console, args);
+        console.warn.apply(console, [`[${new Date().toLocaleString()}]`, ...args]);
     }
     error(...args: any[]): void {
-        console.error.apply(console, args);
+        console.error.apply(console, [`[${new Date().toLocaleString()}]`, ...args]);
     }
 }
