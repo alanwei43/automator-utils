8411556a1dfe9f13dec29396021a2c801333f7ad
alan <alan.wei@live.com>
2021-08-17 18:57:14 +0800

    refactor: 修改类型定义

--- src/automator/StepMiddleware.ts
+++ src/automator/StepMiddleware.ts
@@ -12,7 +12,7 @@ export abstract class StepMiddleware implements OnionMiddleware<StepMiddlewareCo
     abstract execute(next: NextMiddleware<StepMiddleware>, ctx: StepMiddlewareContext, ...args: Array<any>): Promise<any>
 }
 
-export interface StepMiddlewareContext extends IMapDeep<BaseTypes | IHttpClient | ILogger> {
+export interface StepMiddlewareContext extends IMapDeep<any> {
     http?: IHttpClient
     logger?: ILogger
 }
